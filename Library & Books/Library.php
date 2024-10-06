863e88222dffc4b4547b6566c495d444646ffc19
Balazs92 <160589756+Balazs92@users.noreply.github.com>
2024-10-06 19:00:15 +0300

    Add files via upload

---
+++ Library & Books/Library.php
@@ -0,0 +1,67 @@
+<?php
+require_once "AbstractLibrary.php";
+require_once "Author.php"; 
+require_once "Book.php";
+
+class Library extends AbstractLibrary
+{
+    private array $authors = [];
+
+    public function addAuthor(string $authorName): Author
+    {
+        
+        if (isset($this->authors[$authorName])) {
+            return $this->authors[$authorName];
+        }
+    
+        
+        $author = new Author($authorName);
+        $this->authors[$authorName] = $author;
+        return $author;
+    }
+
+    
+    public function addBookForAuthor($authorName, Book $book)
+    {
+        if (isset($this->authors[$authorName])) {
+            $author = $this->authors[$authorName];
+            $author->addBook($book->getTitle(), $book->getPrice());
+            $book->setAuthor($author);
+        }
+    }
+
+    
+    public function getBooksForAuthor($authorName)
+    {
+        if (isset($this->authors[$authorName])) {
+            return $this->authors[$authorName]->getBooks();
+        }
+        return [];
+    }
+
+    
+    public function search(string $bookName): Book
+    {
+        foreach ($this->authors as $author) {
+            foreach ($author->getBooks() as $book) {
+                if ($book->getTitle() === $bookName) {
+                    return $book;
+                }
+            }
+        }
+        throw new Exception("Book not found.");
+    }
+
+    
+    public function print()
+    {
+        foreach ($this->authors as $author) {
+            echo $author->getName() . "<br>";
+            echo "----------------------<br>";
+            foreach ($author->getBooks() as $book) {
+                echo $book->getTitle() . " - " . $book->getPrice() . "<br>";
+            }
+            echo "<br>";
+        }
+    }
+}
